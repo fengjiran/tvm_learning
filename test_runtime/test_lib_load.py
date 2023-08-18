3b55e3beef86eb1cc7b870f322a49ca6ee27369f
fengjiran <fengjiran@foxmail.com>
2023-08-18 20:42:54 +0800

    list tvm lib and header file path

--- test_runtime/test_lib_load.py
+++ test_runtime/test_lib_load.py
@@ -6,12 +6,14 @@ class TestTVMLibLoad(unittest.TestCase):
     def test_find_tvm_lib(self):
         lib_path = libinfo.find_lib_path()
         self.assertNotEqual(len(lib_path), 0, 'tvm libs not found.')
+        print('List tvm libs:')
         for path in lib_path:
             print(path)
 
     def test_find_include_path(self):
         include_path = libinfo.find_include_path()
         self.assertNotEqual(len(include_path), 0, 'tvm header files for C compilation not found.')
+        print('List tvm include path:')
         for path in include_path:
             print(path)
 
