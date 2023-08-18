83386d1eed0ea606413ba3a3dfd109d2b2d76254
fengjiran <fengjiran@foxmail.com>
2023-08-18 16:18:55 +0800

    test runtime

--- test_runtime/test_lib_load.py
+++ test_runtime/test_lib_load.py
@@ -2,15 +2,12 @@ import unittest
 from tvm._ffi import libinfo
 
 
-def find_tvm_lib_path(name=None, search_path=None, optional=False):
-    lib_path = libinfo.find_lib_path(name, search_path, optional)
-    return lib_path
-
-
 class TestTVMLibLoad(unittest.TestCase):
-    def test_load_tvm_lib(self):
-        lib_path = find_tvm_lib_path()
-        self.assertNotEqual(len(lib_path), 0)
+    def test_find_tvm_lib(self):
+        lib_path = libinfo.find_lib_path()
+        self.assertNotEqual(len(lib_path), 0, 'Not found tvm libs.')
+        for path in lib_path:
+            print(path)
 
 
 # https://www.cnblogs.com/miki-peng/p/12501341.html
