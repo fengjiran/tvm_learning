3d218e92ba4bba08f5600be44ee586fdbd0192a1
fengjiran <fengjiran@foxmail.com>
2023-08-18 11:38:01 +0800

    test tvm lib load

--- test_lib_load.py
+++ test_lib_load.py
@@ -1,5 +1,17 @@
 import unittest
+from tvm._ffi import libinfo
+
+
+def find_tvm_lib_path(name=None, search_path=None, optional=False):
+    lib_path = libinfo.find_lib_path(name, search_path, optional)
+    return lib_path
 
 
 class TestTVMLibLoad(unittest.TestCase):
-    pass
+    def test_load_tvm_lib(self):
+        lib_path = find_tvm_lib_path()
+        self.assertIsNotNone(lib_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
