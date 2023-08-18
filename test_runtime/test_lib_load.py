0e411c62597df8e6d97e1e34d806d50a5c3f926f
fengjiran <fengjiran@foxmail.com>
2023-08-18 20:38:22 +0800

    add assert failure msg

--- test_runtime/test_lib_load.py
+++ test_runtime/test_lib_load.py
@@ -5,13 +5,13 @@ from tvm._ffi import libinfo
 class TestTVMLibLoad(unittest.TestCase):
     def test_find_tvm_lib(self):
         lib_path = libinfo.find_lib_path()
-        self.assertNotEqual(len(lib_path), 0, 'Not found tvm libs.')
+        self.assertNotEqual(len(lib_path), 0, 'tvm libs not found.')
         for path in lib_path:
             print(path)
 
     def test_find_include_path(self):
         include_path = libinfo.find_include_path()
-        self.assertNotEqual(len(include_path), 0, 'Not found tvm header files for C compilation.')
+        self.assertNotEqual(len(include_path), 0, 'tvm header files for C compilation not found.')
         for path in include_path:
             print(path)
 
