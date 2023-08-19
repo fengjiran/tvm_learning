647cc77e0e3c3f4b3a56b22cc3744b291332efab
fengjiran <fengjiran@foxmail.com>
2023-08-19 10:35:32 +0800

    test global func

--- test_runtime/test_global_func.py
+++ test_runtime/test_global_func.py
@@ -1,4 +1,6 @@
 import unittest
+
+import tvm
 from tvm import get_global_func
 from tvm._ffi.registry import list_global_func_names
 
@@ -7,6 +9,7 @@ class TestGlobalFunc(unittest.TestCase):
     def test_get_global_func(self):
         func = get_global_func("relay._transform.FoldConstantExpr")
         self.assertNotEqual(func, None)
+        self.assertIsInstance(func, tvm.runtime.PackedFunc)
 
     @unittest.skip('There are so many global functions.')
     def test_list_global_func_names(self):
