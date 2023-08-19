aef0ac2d7eb30a5c1bfdf3d3a8c99386d12473d0
fengjiran <fengjiran@foxmail.com>
2023-08-20 01:05:22 +0800

    test relay ir

---
+++ test_relay/run_relay_test.py
@@ -0,0 +1,14 @@
+import unittest
+import test_relay_ir
+
+
+def create_suite():
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite.addTest(loader.loadTestsFromModule(test_relay_ir))
+    return suite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(create_suite())
---
+++ test_relay/test_relay_ir.py
@@ -0,0 +1,12 @@
+import unittest
+import tvm
+
+
+def check_json_roundtrip(node):
+    json_str = tvm.ir.save_json(node)
+    back = tvm.ir.load_json(json_str)
+    assert tvm.ir.structural_equal(back, node, map_free_vars=True)
+
+
+class TestRelayIR(unittest.TestCase):
+    pass
