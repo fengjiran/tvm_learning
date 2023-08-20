3c4db97bdac6d18d15c583afa3bdc17570db4453
fengjiran <fengjiran@foxmail.com>
2023-08-20 19:29:29 +0800

    test fold constant

--- test_pass/test_fold_constant.py
+++ test_pass/test_fold_constant.py
@@ -5,7 +5,12 @@ from tvm import relay
 
 
 def run_opt_pass(expr, opt_pass):
-    pass
+    assert isinstance(opt_pass, tvm.transform.Pass)
+    mod = tvm.IRModule.from_expr(expr)
+    mod = relay.transform.InferType()(mod)
+    mod = opt_pass(mod)
+    entry = mod['main']
+    return entry if isinstance(expr, relay.Function) else entry.body
 
 
 def annot_expr(expr):
@@ -13,7 +18,18 @@ def annot_expr(expr):
 
 
 class TestFoldConstant(unittest.TestCase):
-    pass
+    def test_fold_constant(self):
+        c_data = np.array([1, 2, 3]).astype('float32')
+        t = relay.TensorType([1, 2, 3], 'float32')
+
+        def befor():
+            c = relay.const(c_data)
+            x = relay.var('x', t)
+            y = relay.add(c, c)
+            y = relay.multiply(y, relay.const(2, 'float32'))
+            y = relay.add(x, y)
+            z = relay.add(y, c)
+            return relay.Function([x], z)
 
 
 if __name__ == '__main__':
