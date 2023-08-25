0a85f60d3aaadd5a72a4d9b4323e93cae3d3fcf4
fengjiran <fengjiran@foxmail.com>
2023-08-25 16:26:38 +0800

    test tvm te tensor

--- test_te/test_te_tensor.py
+++ test_te/test_te_tensor.py
@@ -4,5 +4,12 @@ import tvm
 from tvm import te
 
 
+class TestTE(unittest.TestCase):
+    def test_tensor(self):
+        m = te.size_var('m')
+        n = te.size_var('n')
+        l = te.size_var('l')
+
+
 if __name__ == '__main__':
     unittest.main()
