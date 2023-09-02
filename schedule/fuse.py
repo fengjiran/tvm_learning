0db861e349f1cd3212db2f00e9830a9b29df909a
fengjiran <fengjiran@foxmail.com>
2023-09-02 12:11:11 +0800

    test schedule split

--- schedule/fuse.py
+++ schedule/fuse.py
@@ -7,7 +7,7 @@ A = te.placeholder((n,), name='A')
 B = te.compute((1,), lambda i: te.sum(A[k], axis=k), name='B')
 
 s = te.create_schedule(B.op)
-ko, ki = s[B].split(B.op.reduce_axis[0], factor=32)
+ko, ki = s[B].split(B.op.reduce_axis[0], factor=16)
 print(tvm.lower(s, [A, B], simple_mode=True))
 print('----------------------------cut line-------------------------------')
 
