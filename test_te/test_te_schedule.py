f9fa3b201ffd5bec2d98a875dea238937d829bd6
fengjiran <fengjiran@foxmail.com>
2023-09-04 00:44:16 +0800

    test schedule split

--- test_te/test_te_schedule.py
+++ test_te/test_te_schedule.py
@@ -5,4 +5,13 @@ from tvm import te
 
 class TestTESchedule(unittest.TestCase):
     def test_split(self):
-        pass
+        n = 1024
+        k = te.reduce_axis((0, n), name='k')
+        A = te.placeholder((n,), name="A")
+        T = te.compute((1,), lambda i: te.sum(A[k], axis=k), name='T')
+        s = te.create_schedule(T.op)
+        ko, ki = s[T].split(T.op.reduce_axis[0], factor=16)
+
+
+if __name__ == '__main__':
+    unittest.main()
