8d6aa0ca0a6cec9aa66a7aa3c3073db2d5d63034
fengjiran <fengjiran@foxmail.com>
2023-09-13 16:37:18 +0800

    test schedule of unroll

--- test_te/test_te_schedule.py
+++ test_te/test_te_schedule.py
@@ -121,6 +121,21 @@ class TestTESchedule(unittest.TestCase):
         s[C].vectorize(yi)
         print(tvm.lower(s, [A, B, C], simple_mode=True))
 
+    def test_unroll(self):
+        n = 1024
+        A = te.placeholder((n, n), name='A')
+        B = te.placeholder((n, n), name='B')
+        C = te.compute((n, n), lambda i, j: A[i, j] + B[i, j], name='C')
+
+        s = te.create_schedule(C.op)
+
+        xo, xi = s[C].split(s[C].op.axis[0], factor=4)
+        print(tvm.lower(s, [A, B, C], simple_mode=True))
+        print('----------------------------cut line-------------------------------')
+
+        s[C].unroll(xi)
+        print(tvm.lower(s, [A, B, C], simple_mode=True))
+
 
 if __name__ == '__main__':
     unittest.main()
