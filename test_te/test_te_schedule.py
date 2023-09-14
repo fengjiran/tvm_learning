00c7b9f3b5a6d71c0bf966e54c18c39453bb1c51
fengjiran <fengjiran@foxmail.com>
2023-09-14 10:25:45 +0800

    test schedule of parallel

--- test_te/test_te_schedule.py
+++ test_te/test_te_schedule.py
@@ -149,6 +149,19 @@ class TestTESchedule(unittest.TestCase):
         s[B].bind(ki, te.thread_axis("threadIdx.x"))
         print(tvm.lower(s, [A, B], simple_mode=True))
 
+    def test_parallel(self):
+        m = 1024
+        n = 1024
+        A = te.placeholder((n, m), name='A')
+        k = te.reduce_axis((0, m), name='k')
+        B = te.compute((n,), lambda i: te.sum(A[i, k], axis=k), name='B')
+
+        s = te.create_schedule(B.op)
+        print(tvm.lower(s, [A, B], simple_mode=True))
+        print('----------------------------cut line-------------------------------')
+        s[B].parallel(B.op.reduce_axis[0])
+        print(tvm.lower(s, [A, B], simple_mode=True))
+
 
 if __name__ == '__main__':
     unittest.main()
