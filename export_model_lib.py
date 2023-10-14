22a5d0edc9a55b5c27d13fff6d9321d6d83da5cb
fengjiran <fengjiran@foxmail.com>
2023-10-14 21:54:42 +0800

    build relay resnet model

--- export_model_lib.py
+++ export_model_lib.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from PIL import Image
 from torchvision import transforms
@@ -6,37 +7,66 @@ from tvm import relay
 from tvm.contrib import graph_executor
 import relay_model
 
-bs = 1
-num_class = 1000
-image_shape = (3, 224, 224)
-data_shape = (bs,) + image_shape
-output_shape = (bs, num_class)
 
-img = Image.open('cat.png').resize((224, 224))
-my_preprocess = transforms.Compose(
-    [
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
-img = my_preprocess(img)
-img = np.expand_dims(img, 0)
+class InferenceResnet(object):
+    def __init__(self,
+                 batch_size=1,
+                 image_shape=(3, 224, 224),
+                 print_model=True,
+                 opt_level=2,
+                 target=tvm.target.Target("llvm"),
+                 device=tvm.cpu(0),
+                 lib_path="resnet_lib.tar"
+                 ):
+        self.batch_size = batch_size
+        self.image_shape = image_shape
+        self.print_model = print_model
+        self.opt_level = opt_level
+        self.target = target
+        self.device = device
+        self.lib_path = lib_path
 
-mod, params = relay_model.resnet.get_workload(num_layers=18, batch_size=bs, image_shape=image_shape)
-print(mod.astext(show_meta_data=False))
+    def build(self):
+        if not os.path.exists(self.lib_path):
+            mod, params = relay_model.resnet.get_workload(num_layers=18,
+                                                          batch_size=self.batch_size,
+                                                          image_shape=self.image_shape)
+            if self.print_model:
+                print(mod.astext(show_meta_data=False))
 
-opt_level = 3
-target = tvm.target.Target('llvm')
-dev = tvm.cpu(0)
-# target = tvm.target.cuda()
-with tvm.transform.PassContext(opt_level=opt_level):
-    lib = relay.build(mod, target, params=params)
-# lib.export_library("deploy_lib.tar")
-module = graph_executor.GraphModule(lib['default'](dev))
-module.set_input('data', tvm.nd.array(img.astype('float32')))
-module.run()
-out = module.get_output(0).numpy()
-print(np.argmax(out))
-print('done')
+            with tvm.transform.PassContext(opt_level=self.opt_level):
+                lib = relay.build(mod, self.target, params=params)
+            lib.export_library(self.lib_path)
+
+    def run(self):
+        if not os.path.exists(self.lib_path):
+            raise ValueError("Build model first.")
+
+        height, width = self.image_shape[1:]
+        img = Image.open('cat.png').resize((height, width))
+        my_preprocess = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(height),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        img = my_preprocess(img)
+        img = np.expand_dims(img, 0)
+        input_data = tvm.nd.array(img.astype('float32'))
+
+        lib = tvm.runtime.load_module(self.lib_path)
+        module = graph_executor.GraphModule(lib['default'](self.device))
+        module.set_input('data', input_data)
+        module.run()
+
+        return module.get_output(0).numpy()
+
+
+if __name__ == "__main__":
+    resnet = InferenceResnet()
+    resnet.build()
+    out = resnet.run()
+    print(np.argmax(out))
+    print('done')
