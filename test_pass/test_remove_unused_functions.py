78aed354217b3e35f9773fcf9729b33af558cf1a
fengjiran <fengjiran@foxmail.com>
2023-10-20 19:34:34 +0800

    test remove unused functions

--- test_pass/test_remove_unused_functions.py
+++ test_pass/test_remove_unused_functions.py
@@ -10,8 +10,20 @@ class TestRemoveUnusedFunctions(unittest.TestCase):
         x = relay.var("x", shape=(1, 16))
         mod["main"] = relay.Function([x], x)
         mod = relay.transform.RemoveUnusedFunctions()(mod)
-        l = set([x[0].name_hint for x in mod.functions.items()])
-        self.assertEqual(l, {"main"})
+        gv_name = set([x[0].name_hint for x in mod.functions.items()])
+        self.assertEqual(gv_name, {"main"})
+
+    def test_remove_prelude_funcs_but_ref_funcs(self):
+        mod = tvm.IRModule()
+        x = relay.var("x", shape=(1, 16))
+        id_func = relay.Function([x], x)
+        id_name = relay.GlobalVar("id_func")
+        mod[id_name] = id_func
+
+        mod["main"] = relay.Function([x], id_name(x))
+        mod = relay.transform.RemoveUnusedFunctions()(mod)
+        gv_name = set([x[0].name_hint for x in mod.functions.items()])
+        self.assertEqual(gv_name, {"id_func", "main"})
 
 
 if __name__ == '__main__':
