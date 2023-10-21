bdbb2268983bcca00c8b6d8a1dcff37cd6e1e0b8
fengjiran <fengjiran@foxmail.com>
2023-10-21 14:07:08 +0800

    test simplify inference pass

--- test_pass/test_simplify_inference.py
+++ test_pass/test_simplify_inference.py
@@ -2,11 +2,16 @@ import unittest
 import numpy as np
 import tvm
 from tvm import relay
+from tvm.ir import structural_equal
+from tvm.relay.transform import InferType
+from tvm.relay.transform import SimplifyInference
 
 
 class TestSimplifyInference(unittest.TestCase):
     def test_simplify_batch_norm(self, dtype="float32"):
-        pass
+        self.check(2, 1, 1, dtype)
+        self.check(4, 1, 1, dtype)
+        self.check(4, 0, 3, dtype)
 
     def simple_batch_norm(self, x, gamma, beta, moving_mean, moving_var, axis=1, epsilon=1e-5, shape=None,
                           dtype="float32"):
@@ -19,9 +24,46 @@ class TestSimplifyInference(unittest.TestCase):
             shift = relay.expand_dims(shift, axis=1, num_newaxis=num_newaxis)
         return x * scale + shift
 
-    def check(self, axis, nstep):
+    def check(self, dim, axis, nstep, dtype):
         eps = 0.01
+        ttype1 = relay.TensorType(tuple(10 for _ in range(dim)), dtype)
+        ttype2 = relay.TensorType((10,), dtype)
+        x = relay.var("x", ttype1)
+        beta = relay.var("beta", ttype2)
+        gamma = relay.var("gamma", ttype2)
+        moving_mean = relay.var("moving_mean", ttype2)
+        moving_var = relay.var("moving_var", ttype2)
+        y1 = x
+        y2 = x
+        for _ in range(nstep):
+            y1, _, _ = relay.nn.batch_norm(
+                y1 + relay.const(1, dtype),
+                gamma,
+                beta,
+                moving_mean,
+                moving_var,
+                epsilon=eps,
+                axis=axis
+            )
+            y1 = relay.nn.dropout(y1)
 
+            y2 = self.simple_batch_norm(
+                y2 + relay.const(1, dtype),
+                gamma,
+                beta,
+                moving_mean,
+                moving_var,
+                epsilon=eps,
+                axis=axis,
+                shape=ttype1.shape,
+                dtype=dtype
+            )
+
+        mod = tvm.IRModule.from_expr(y1)
+        mod = InferType()(mod)
+        mod = SimplifyInference()(mod)
+        y1 = mod["main"].body
+        self.assertTrue(structural_equal(y1, y2, map_free_vars=True))
 
 
 if __name__ == '__main__':
