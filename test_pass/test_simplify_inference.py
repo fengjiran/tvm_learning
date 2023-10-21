65e02a684abf28be5712de3d7d0a7c2da5422f83
fengjiran <fengjiran@foxmail.com>
2023-10-21 11:14:36 +0800

    test simplify inference pass

---
+++ test_pass/test_simplify_inference.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+import tvm
+from tvm import relay
+
+
+class TestSimplifyInference(unittest.TestCase):
+    def test_simplify_batch_norm(self, dtype="float32"):
+        pass
+
+    def simple_batch_norm(self, x, gamma, beta, moving_mean, moving_var, axis=1, epsilon=1e-5, shape=None,
+                          dtype="float32"):
+        # expect = (x - moving_mean) / sqrt(moving_var + epsilon) * gamma + beta
+        scale = relay.multiply(relay.const(1, dtype) / relay.sqrt(moving_var + relay.const(epsilon, dtype)), gamma)
+        shift = relay.add(relay.multiply(relay.negative(moving_mean), scale), beta)
+        num_newaxis = len(shape) - (axis + 1)
+        if num_newaxis:
+            scale = relay.expand_dims(scale, axis=1, num_newaxis=num_newaxis)
+            shift = relay.expand_dims(shift, axis=1, num_newaxis=num_newaxis)
+        return x * scale + shift
+
+
+if __name__ == '__main__':
+    unittest.main()
