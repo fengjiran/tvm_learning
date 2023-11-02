c3de19ff9342ad6318ee4cb94741bbe9e9d076f3
fengjiran <fengjiran@foxmail.com>
2023-11-02 20:30:22 +0800

    add op pattern dict for debug

--- test_pass/test_fuse_ops.py
+++ test_pass/test_fuse_ops.py
@@ -3,26 +3,25 @@ import numpy as np
 import tvm
 from tvm import relay
 
+opPatternDict = {
+    0: "kElemWise",
+    1: "kBroadcast",
+    2: "kInjective",
+    3: "kCommReduce",
+    4: "kOutEWiseFusable",
+    7: "kTuple",
+    8: "kOpaque"
+}
+
 
 class TestFuseOps(unittest.TestCase):
-    def __init__(self):
-        super().__init__()
-        self.opPatternDict = {
-            0: "kElemWise",
-            1: "kBroadcast",
-            2: "kInjective",
-            3: "kCommReduce",
-            4: "kOutEWiseFusable",
-            7: "kTuple",
-            8: "kOpaque"
-        }
-
     def test_fuse_simple(self):
         def before():
             x = relay.var("x", shape=(10, 20))
             y = relay.add(x, relay.const(1, "float32"))
             z = relay.exp(y)
             w = relay.squeeze(z)
+            # print(opPatternDict[w.op.get_attr("TOpPattern")])
             return relay.Function([x], w)
 
         z = before()
