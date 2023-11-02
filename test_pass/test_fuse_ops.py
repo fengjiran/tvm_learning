955118050ac444ec8c4fdff434b3e9ebcedded40
fengjiran <fengjiran@foxmail.com>
2023-11-03 00:19:47 +0800

    test fuse ops

--- test_pass/test_fuse_ops.py
+++ test_pass/test_fuse_ops.py
@@ -4,5 +4,18 @@ import tvm
 from tvm import relay
 
 
+class TestFuseOps(unittest.TestCase):
+    def test_fuse_simple(self):
+        def before():
+            x = relay.var("x", shape=(10, 20))
+            y = relay.add(x, relay.const(1, "float32"))
+            z = relay.exp(y)
+            w = relay.squeeze(z)
+            return relay.Function([x], w)
+
+        z = before()
+        print(z)
+
+
 if __name__ == '__main__':
     unittest.main()
