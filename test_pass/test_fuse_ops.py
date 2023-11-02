ba19bba2469b940816ae62daaebd2affb71a730b
fengjiran <fengjiran@foxmail.com>
2023-11-02 20:15:44 +0800

    add op pattern dict for debug

--- test_pass/test_fuse_ops.py
+++ test_pass/test_fuse_ops.py
@@ -5,6 +5,18 @@ from tvm import relay
 
 
 class TestFuseOps(unittest.TestCase):
+    def __init__(self):
+        super().__init__()
+        self.opPatternDict = {
+            0: "kElemWise",
+            1: "kBroadcast",
+            2: "kInjective",
+            3: "kCommReduce",
+            4: "kOutEWiseFusable",
+            7: "kTuple",
+            8: "kOpaque"
+        }
+
     def test_fuse_simple(self):
         def before():
             x = relay.var("x", shape=(10, 20))
