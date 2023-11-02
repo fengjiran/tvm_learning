17e27b875dd6c5a4cbbc4d1bc530addbd5dbbbf4
fengjiran <fengjiran@foxmail.com>
2023-11-02 21:12:43 +0800

    test fuse ops pass

--- test_pass/test_fuse_ops.py
+++ test_pass/test_fuse_ops.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import tvm
 from tvm import relay
+from test_fold_constant import run_opt_pass
 
 opPatternDict = {
     0: "kElemWise",
@@ -21,11 +22,26 @@ class TestFuseOps(unittest.TestCase):
             y = relay.add(x, relay.const(1, "float32"))
             z = relay.exp(y)
             w = relay.squeeze(z)
-            # print(opPatternDict[w.op.get_attr("TOpPattern")])
+            print(y.op.name + " Pattern: " + opPatternDict[y.op.get_attr("TOpPattern")])
+            print(z.op.name + " Pattern: " + opPatternDict[z.op.get_attr("TOpPattern")])
+            print(w.op.name + " Pattern: " + opPatternDict[w.op.get_attr("TOpPattern")])
             return relay.Function([x], w)
 
+        def expected():
+            x = relay.var("p", shape=(10, 20))
+            y = relay.add(x, relay.const(1, "float32"))
+            z = relay.exp(y)
+            w = relay.squeeze(z)
+            f1 = relay.Function([x], w)
+            f1 = f1.with_attr("Primitive", tvm.tir.IntImm("int32", 1))
+            x = relay.var("x", shape=(10, 20))
+            y = relay.Call(f1, [x])
+            return relay.Function([x], y)
+
         z = before()
-        print(z)
+        zz = run_opt_pass(z, relay.transform.FuseOps())
+        after = run_opt_pass(expected(), relay.transform.InferType())
+        self.assertTrue(tvm.ir.structural_equal(zz, after))
 
 
 if __name__ == '__main__':
