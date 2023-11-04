b49d38553a6ec99c53930fac1743b2b2558a6ca3
fengjiran <fengjiran@foxmail.com>
2023-11-04 09:36:39 +0800

    test conv2d fuse

--- test_pass/test_fuse_ops.py
+++ test_pass/test_fuse_ops.py
@@ -44,7 +44,62 @@ class TestFuseOps(unittest.TestCase):
         self.assertTrue(tvm.ir.structural_equal(zz, after))
 
     def test_conv2d_fuse(self):
-        pass
+        def before(shape):
+            x = relay.var("x", shape=shape)
+            x = relay.add(x, relay.const(1, "float32"))
+            y = relay.nn.conv2d(x, relay.var("w1"), kernel_size=(3, 3), padding=(1, 1), channels=16)
+            y1 = relay.add(y, relay.const(1, "float32"))
+            y = relay.add(y, y1)
+            z2 = relay.nn.conv2d(y, relay.var("w2"), kernel_size=(1, 1), padding=(0, 0), channels=16)
+            z3 = relay.nn.conv2d(y, relay.var("w2"), kernel_size=(3, 3), padding=(1, 1), channels=16)
+            z = relay.add(z2, z3)
+            return relay.Function(relay.analysis.free_vars(z), z)
+
+        def expected(shape):
+            # segment0
+            x = relay.var("p0", shape=shape)
+            y = relay.add(x, relay.const(1, "float32"))
+            f0 = relay.Function([x], y)
+            f0 = f0.with_attr("Primitive", tvm.tir.IntImm("int32", 1))
+
+            # segment1
+            x = relay.var("p0", shape=shape)
+            w = relay.var("p1")
+            y = relay.nn.conv2d(x, w, kernel_size=(3, 3), padding=(1, 1), channels=16)
+            y1 = relay.add(y, relay.const(1, "float32"))
+            y = relay.add(y, y1)
+            f1 = relay.Function([x, w], y)
+            f1 = f1.with_attr("Primitive", tvm.tir.IntImm("int32", 1))
+
+            # segment 2
+            x = relay.var("p0", shape=shape)
+            w = relay.var("p1")
+            z2 = relay.nn.conv2d(x, w, kernel_size=(3, 3), padding=(1, 1), channels=16)
+            f2 = relay.Function([x, w], z2)
+            f2 = f2.with_attr("Primitive", tvm.tir.IntImm("int32", 1))
+
+            # segment 3
+            x = relay.var("p0", shape=shape)
+            w = relay.var("p1")
+            offset = relay.var("p2", shape=shape)
+            z3 = relay.nn.conv2d(x, w, kernel_size=(1, 1), padding=(0, 0), channels=16)
+            z3 = relay.add(z3, offset)
+            f3 = relay.Function([x, w, offset], z3)
+            f3 = f3.with_attr("Primitive", tvm.tir.IntImm("int32", 1))
+
+            # compose
+            x = relay.var("x", shape=shape)
+            y = relay.Call(f0, [x])
+            y = relay.Call(f1, [y, relay.var("w1")])
+            z2 = relay.Call(f2, [y, relay.var("w3")])
+            z3 = relay.Call(f3, [y, relay.var("w2"), z2])
+            return relay.Function(relay.analysis.free_vars(z3), z3)
+
+        shape = (1, 16, 64, 64)
+        z = before(shape)
+        zz = run_opt_pass(z, relay.transform.FuseOps(2))
+        after = run_opt_pass(expected(shape), relay.transform.InferType())
+        self.assertTrue(tvm.ir.structural_equal(zz, after))
 
 
 if __name__ == '__main__':
