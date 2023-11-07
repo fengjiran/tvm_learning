0a8a8e8f8c9e964b31684e88ca3e4c607f4d374e
fengjiran <fengjiran@foxmail.com>
2023-11-07 18:50:42 +0800

    test injective op strategy

---
+++ test_op_strategy/test_injective_op_strategy.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+import tvm
+from tvm import relay
+
+
+class TestInjectiveOpStrategy(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
