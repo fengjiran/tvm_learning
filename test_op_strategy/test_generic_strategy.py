77e43e8d98458d7a3b48934c99da4956555cf451
fengjiran <fengjiran@foxmail.com>
2023-11-09 21:34:09 +0800

    test relu strategy

--- test_op_strategy/test_generic_strategy.py
+++ test_op_strategy/test_generic_strategy.py
@@ -7,8 +7,24 @@ from tvm.relay.testing import run_infer_type
 
 
 class TestGenericStrategy(unittest.TestCase):
+    def test_relu_strategy(self):
+        target = tvm.target.Target("llvm")
+        dshape = (10, 3, 256, 256)
+        op = relay.op.get("nn.relu")
+        data = relay.var("data", shape=dshape)
+        out = relay.nn.relu(data)
+        out = run_infer_type(out)
+        impl = relay.backend.te_compiler.get_valid_implementations(
+            op,
+            out.attrs,
+            [te.placeholder(dshape)],
+            out.checked_type,
+            target
+        )
+        return impl
+
     def test_softmax_strategy(self):
-        target = tvm.target.Target("cuda")
+        target = tvm.target.Target("llvm")
         shape = (10, 100)
         op = relay.op.get("nn.softmax")
         data = relay.var("data", shape=shape)
