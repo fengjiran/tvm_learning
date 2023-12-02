07e4833ee03b42b2a9fc6c760b486dabf5527fe3
fengjiran <fengjiran@foxmail.com>
2023-12-02 17:05:33 +0800

    test self attention

--- test_transformer/self_attention.py
+++ test_transformer/self_attention.py
@@ -1,3 +1,5 @@
+import math
+import torch
 from torch import nn
 from transformers import AutoConfig
 from transformers import AutoTokenizer
@@ -9,3 +11,13 @@ tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 text = "time flies like an arrow"
 inputs = tokenizer(text, return_tensors="pt", add_special_tokens=False)
 print(inputs.input_ids)
+
+config = AutoConfig.from_pretrained(model_ckpt)
+token_emb = nn.Embedding(config.vocab_size, config.hidden_size)
+print(token_emb)
+
+inputs_embeds = token_emb(inputs.input_ids)
+print(inputs_embeds.size())
+
+q = k = v = inputs_embeds
+
