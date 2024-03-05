80d8ac5399e3f081e751b99fbf8510262564fe31
fengjiran <fengjiran@foxmail.com>
2024-03-06 02:56:18 +0800

    rename

--- test_transformer/attention.py
+++ test_transformer/attention.py
@@ -33,7 +33,6 @@ if __name__ == "__main__":
         model_ckpt = "/home/richard/.cache/huggingface/hub/models--bert-base-uncased/"
     else:
         raise ValueError("Unsupported platform")
-    #
 
     tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 
