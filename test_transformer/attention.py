157dc162ca6485aa1843b7e2fd2ed2af7e0a1133
fengjiran <fengjiran@foxmail.com>
2024-03-12 00:29:48 +0800

    attention

--- test_transformer/attention.py
+++ test_transformer/attention.py
@@ -1,25 +1,9 @@
+import sys
 import math
 import torch
 from torch import nn
 from transformers import AutoConfig
 from transformers import AutoTokenizer
-
-
-model_ckpt = "/Users/richard/.cache/huggingface/hub/models--bert-base-uncased/"
-tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-
-text = "time flies like an arrow"
-inputs = tokenizer(text, return_tensors="pt", add_special_tokens=False)
-print(inputs.input_ids)
-
-config = AutoConfig.from_pretrained(model_ckpt)
-token_emb = nn.Embedding(config.vocab_size, config.hidden_size)
-print(token_emb)
-
-inputs_embeds = token_emb(inputs.input_ids)
-print(inputs_embeds.size())
-
-q = k = v = inputs_embeds
 
 
 class AttentionHead(nn.Module):
@@ -31,4 +15,34 @@ class AttentionHead(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, query, key, value, mask=None):
-        pass
+        query = self.q(query)
+        key = self.k(key)
+        value = self.v(value)
+        scale = torch.sqrt(query.size(-1))
+        scores = torch.bmm(query, key.transpose(1, 2)) / scale
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -float("inf"))
+        weights = self.softmax(scores)
+        return torch.bmm(weights, value)
+
+
+if __name__ == "__main__":
+    if sys.platform.startswith("darwin"):
+        model_ckpt = "/Users/richard/.cache/huggingface/hub/models--bert-base-uncased/"
+    elif sys.platform.startswith("linux"):
+        model_ckpt = "/home/richard/.cache/huggingface/hub/models--bert-base-uncased/"
+    else:
+        raise ValueError("Unsupported platform")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+
+    text = "time flies like an arrow"
+    inputs = tokenizer(text, return_tensors="pt", add_special_tokens=False)
+    print(inputs.input_ids)
+
+    config = AutoConfig.from_pretrained(model_ckpt)
+    token_emb = nn.Embedding(config.vocab_size, config.hidden_size)
+    print(token_emb)
+
+    inputs_embeds = token_emb(inputs.input_ids)
+    print(inputs_embeds.size())
