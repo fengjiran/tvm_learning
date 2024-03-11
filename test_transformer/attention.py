3096616bacc17e1803e289a42c0622d7e5ae92f5
fengjiran <fengjiran@foxmail.com>
2024-03-11 23:32:50 +0800

    attention

--- test_transformer/attention.py
+++ test_transformer/attention.py
@@ -21,3 +21,14 @@ print(inputs_embeds.size())
 
 q = k = v = inputs_embeds
 
+
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim, head_dim):
+        super().__init__()
+        self.q = nn.Linear(embed_dim, head_dim)
+        self.k = nn.Linear(embed_dim, head_dim)
+        self.v = nn.Linear(embed_dim, head_dim)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, query, key, value, mask=None):
+        pass
